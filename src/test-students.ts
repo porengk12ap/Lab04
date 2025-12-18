1c17b1296fc92733921021c5f5ff80bbbc552d94
porengk12ap <116424779+porengk12ap@users.noreply.github.com>
2025-12-18 14:48:45 +0700

    Add files via upload

---
+++ src/test-students.ts
@@ -0,0 +1,7 @@
+import { loadStudents, saveStudents, addStudent } from "./services/studentService"; 
+ 
+const students = loadStudents(); 
+const updated = addStudent(students, "Mina", "UX"); 
+saveStudents(updated); 
+ 
+console.log("Updated students:", updated);
